79d1b35084d08a98317c70aa9f271f1b7a080950
Andrew Lau <an.lau23@gmail.com>
2016-12-13 22:19:41 +0000

    Initial add

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <button className="ui button">Follow</button>
+      <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
