cb86d16664514cceb09f3649740d5270b4a0a669
Andrew Lau <an.lau23@gmail.com>
2016-12-14 22:02:13 +0000

    Putting in a divider between input/output. Adding a results page.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import './App.css';
 import Picker from './Picker';
 import Manager from './Manager';
+import Results from './Results';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-	<div className="ui top attached tabular menu">
-          <a className="item" data-tab="first">Pick Meal</a>
-
-	  <a className="item active" data-tab="second">Manage</a>
- 
-        </div>
-        <div className="ui bottom attached tab segment" data-tab="first">
-          <Picker />
-        </div>
-        <div className="ui bottom attached tab segment active" data-tab="second">
-	  <Manager />
-        </div>
+      <div className="ui two column middle aligned very relaxed stackable grid">
+	<div className="column">
+	  <div className="ui top attached tabular menu">
+            <a className="item" data-tab="first">Pick Meal</a>
+	    <a className="item active" data-tab="second">Manage</a>
+          </div>
+          <div className="ui bottom attached tab segment" data-tab="first">
+            <Picker />
+          </div>
+          <div className="ui bottom attached tab segment active" data-tab="second">
+	    <Manager />
+          </div>
+	</div>
+	<div className="ui vertical divider"> </div>
+	<div className="center aligned column">
+	  <Results />
+	</div>
       </div> 
     );
   }
