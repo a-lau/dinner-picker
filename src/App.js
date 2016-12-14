17d0c26cb0189f64dfa649a49113b2c7e41eac2e
Andrew Lau <an.lau23@gmail.com>
2016-12-14 22:19:38 +0000

    Fiddling around with layout again. Adding some headers/icons.

--- src/App.js
+++ src/App.js
@@ -7,24 +7,31 @@ import Results from './Results';
 export default class App extends React.Component {
   render() {
     return (
-      <div className="ui two column middle aligned very relaxed stackable grid">
-	<div className="column">
-	  <div className="ui top attached tabular menu">
-            <a className="item" data-tab="first">Pick Meal</a>
-	    <a className="item active" data-tab="second">Manage</a>
-          </div>
-          <div className="ui bottom attached tab segment" data-tab="first">
-            <Picker />
-          </div>
-          <div className="ui bottom attached tab segment active" data-tab="second">
-	    <Manager />
-          </div>
-	</div>
-	<div className="ui vertical divider"> </div>
-	<div className="center aligned column">
-	  <Results />
-	</div>
-      </div> 
+      <div>
+        <h2 className="ui header">
+         <div className="content">
+           Dinner Picker 
+         </div>
+        </h2>
+        <div className="ui two column middle aligned very relaxed stackable grid">
+	  <div className="column">
+	    <div className="ui top attached tabular menu">
+              <a className="item" data-tab="first">Pick Meal</a>
+	      <a className="item active" data-tab="second">Manage</a>
+            </div>
+            <div className="ui bottom attached tab segment" data-tab="first">
+              <Picker />
+            </div>
+            <div className="ui bottom attached tab segment active" data-tab="second">
+  	      <Manager />
+            </div>
+	  </div>
+	  <div className="ui vertical divider"> </div>
+	  <div className="center aligned column">
+	    <Results />
+	  </div>
+        </div> 
+      </div>
     );
   }
 }
--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default class Results extends React.Component {
   render() {
     return (
-      <h1>Results</h1>
+      <h2 className="ui center aligned icon header">
+	<i className="circular shopping basket icon"></i>
+	Results
+      </h2>
     );
   }
 }
