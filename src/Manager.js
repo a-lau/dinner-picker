1d389cd5ab6980aa005afd90722dc9632999d0a4
Andrew Lau <an.lau23@gmail.com>
2016-12-22 23:45:45 +0000

    Using simple for now to avoid javascript problems of dropdown not expanding

--- src/Manager.js
+++ src/Manager.js
@@ -55,57 +55,21 @@ export default class Manager extends React.Component {
   	  <div>
 	    {this.listItems()}
 	  </div>
-	  <div className="ui floating labeled icon dropdown button">
+	  <div className="ui icon simple labeled dropdown button">
 	    <i className="plus icon"></i>
 	    <span className="text">Add New Item</span>
 	    <div className="menu">
 	      <div className="header">Input New Item</div>
-	      <div className="ui left icon input">
-	      	<i className="plus icon"></i>
+	      <div className="ui left input">
                 <input type="text" name="meal-type" placeholder="Enter a meal option..."></input>
+		<button className="ui button" onClick={this.clickAdd}>Add</button>
               </div> 
-	      <div className="item">
-                <button className="ui button" onClick={this.clickAdd}>Add</button>
-	      </div>
             </div>
           </div>
           <button className="ui button" onClick={this.clickEdit}>Edit</button>
 	  <button className="ui button" onClick={this.clickDelete}>Delete Item</button>
-	 
-<div class="ui floating labeled icon dropdown button">
-  <i class="filter icon"></i>
-    <span class="text">Filter</span>
-      <div class="menu">
-          <div class="header">
-	        Search Issues
-		    </div>
-		        <div class="ui left icon input">
-			      <i class="search icon"></i>
-			            <input type="text" name="search" placeholder="Search...">
-				        </div>
-					    <div class="header">
-					          <i class="tags icon"></i>
-						        Filter by tag
-							    </div>
-							        <div class="divider"></div>
-								    <div class="item">
-								          <div class="ui red empty circular label"></div>
-									        Important
-										    </div>
-										        <div class="item">
-											      <div class="ui blue empty circular label"></div>
-											            Announcement
-												        </div>
-													    <div class="item">
-													          <div class="ui black empty circular label"></div>
-														        Discussion
-															    </div>
-															      </div>
-															      </div>
-
-
-
         </div>
       );
+
     }
 }
