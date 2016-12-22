19c218731fde6c2e92608cc035bd4f8db39af133
Andrew Lau <an.lau23@gmail.com>
2016-12-22 02:54:03 +0000

    Broken

--- src/Manager.js
+++ src/Manager.js
@@ -34,18 +34,21 @@ export default class Manager extends React.Component {
       });
     }
     listItems() {
-	console.log("hello?")
-	console.log( this.state.foodData )
+      console.log("hello?")
+      console.log( this.state.foodData )
       return (
-	<div className="item">
-	  <div className="content">
-	    {this.state.foodData.map(function(item) {
-              return <div className="header" key={item.name}>{item.name}</div>
-            })}
-  	  </div>
-	</div>
+        {this.state.foodData.map(function(item) {
+          return( 
+            <div className="item">
+              <div className="content">
+	        <div className="header" key={item.name}>{item.name}</div>
+	      </div>
+	    </div>
+	  )
+        })}
       )
     }
+
     render() {
       console.log( this.state.foodData )
       return(
