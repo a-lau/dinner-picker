bf479279b509e2ebeb7c1987a95e71c66dbec951
Andrew Lau <an.lau23@gmail.com>
2017-01-23 18:16:23 +0000

    Switching over to a table for proper formatting

--- src/EatenList.js
+++ src/EatenList.js
@@ -18,11 +18,22 @@ export default class EatenList extends React.Component {
     if(!!!this.state.jsonEaten) {
       return <div> Loading... </div>
     } else {
-    return (<div className="ui middle aligned selection list">
-      {this.state.jsonEaten.map(function(item) {
-        return <div key={item.key}>{item.name}</div>
-      }, this)}
-    </div> )
-    }
+    return (
+      <table className="ui striped table">
+        <thead>
+	  <tr><th className="eight wide">Name</th>
+	  <th className="four wide">Date</th>
+	</tr></thead>
+        <tbody>
+        {this.state.jsonEaten.map(function(item) {
+          return (
+	    <tr key={item.key}>
+	      <td>{item.name}</td>
+	      <td>{item.dateUsed}</td>
+	    </tr>
+	 )}, this)}
+	</tbody>
+      </table>
+   )}
   }
 }
