f6214cd27102834328adf4e4c7155e393779d55b
Andrew Lau <an.lau23@gmail.com>
2017-01-26 05:26:12 +0000

    Adding in redux package and some tools

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import client from './client.js';
 import './index.css';
 import '../semantic/dist/semantic.min.css';
 
 ReactDOM.render(
-  <App />,
+  <client />,
   document.getElementById('root')
 );
