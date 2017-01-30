d96849f0c1bd1045be4f788995fb5d746e73b4da
Andrew Lau <an.lau23@gmail.com>
2017-01-30 04:23:23 +0000

    slight re-org, removing extra console logs

--- src/reducers/tabReducer.js
+++ src/reducers/tabReducer.js
@@ -6,8 +6,6 @@ const initialState= {
 const TabReducer = (state=initialState, action) => {
   switch (action.type) {
     case "SET_CLICKED_TAB": {
-			console.log("actions file")
-					console.log(action.payload)
       return {...state, tabClicked: action.payload}
     }
     default:
