ec4a209e3b553dd4c4ee14cba17db415ad301703
Andrew Lau <an.lau23@gmail.com>
2017-03-09 18:08:06 +0000

    Fixed last had being invalid date

--- src/components/Results.js
+++ src/components/Results.js
@@ -28,10 +28,17 @@ class Results extends React.Component {
     if( this.props.displayedFood.key === undefined ) {
       return null
     } else {
-      var newDate = new Date(this.props.displayedFood.date).toString().substring(0,16)
+		console.log(this.props)
+		var dateCheck = this.props.displayedFood.date
+        var lastHad = null
+      if(dateCheck.isNaN) {
+		lastHad = new Date(dateCheck).toString().substring(0,16)
+	  } else {
+		lastHad = dateCheck
+	  }
       return (
         <div>
-          <h5 className="ui center aligned header"> Last had: {newDate} </h5>
+          <h5 className="ui center aligned header"> Last had: {lastHad} </h5>
           <div className="div_button">
             <SelectButton resetButton={this.resetButton} buttonText={this.state.buttonText} green={this.state.green} updateButton={this.updateButton} />
           </div>
